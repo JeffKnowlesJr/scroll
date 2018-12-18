f738fd444d914f8aafad3e3d49f2c7230953bd84
Jefek <jeffrey.knowles@gmail.com>
2018-12-18 11:03:53 -0500

    Updated Post Model

--- server/models/post.js
+++ server/models/post.js
@@ -2,18 +2,30 @@ console.log("inside of post.js");
 
 const mongoose = require("mongoose");
 
-const PostSchema = new mongoose.Schema({
-    name: {type: String, required: [true, "Poster must have a name"], maxlength:255},
-    rating: {type: Number, required: [true, "Poster must enter a rating"], min:0, max:5},
-    post: {type: String, required: [true, "Poster must enter a post"], minlength:10, maxlength:255},
-}, {timestamps: true});
+const PostSchema = new mongoose.Schema(
+  {
+    title: {
+    // Post Model Attribute Title
+      type: String,
+      required: [true, "Post must have a title."],
+      maxlength: [255, "Title must be less than 255 characters."]
+    },
+    // Post Model Attribute Contents
+    rating: {
+      type: String,
+      required: [true, "Post must have contents."],
+      maxlength: [2000, "Contents must be less than 2000 characters."],
+      minlength: [5, "Contents must be at least 5 characters"]
+    },
+    // Post Model Attribute Chat_id
+    chat_id: {
+      type: String
+    }
+  },
+  {timestamps: true}
+);
 
 mongoose.model('Post', PostSchema);
 
-// required for post to be accessable from user.js
+// Required for post to be accessable from user.js
 module.exports = PostSchema;
-
-// Post
-//   poster Name
-//   Rating
-//   Post

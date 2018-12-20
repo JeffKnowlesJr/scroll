88dea1508a550ac7388b1370f1eed7f9edb21dba
Jefek <jeffrey.knowles@gmail.com>
2018-12-20 17:05:32 -0500

    Added Upload Component

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,11 +3,11 @@ import { NgModule } from '@angular/core';
 import { UserService } from './user.service';
 import { PostService } from './post.service';
 import { CommentService } from './comment.service';
-import { ChatService } from './chat.service';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { UploadModule } from './upload/upload.module';
 import { HomeComponent } from './home/home.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { LoginComponent } from './login/login.component';
@@ -29,12 +29,13 @@ import { AppRoutingModule } from './app-routing.module';
     HttpClientModule,
     FormsModule,
     AppRoutingModule,
+    UploadModule
   ],
   providers: [UserService],
 
   // providers: [PostService],
   // providers: [CommentService],
-  // providers: [ChatService],
+
   bootstrap: [AppComponent]
 })
 export class AppModule { }
